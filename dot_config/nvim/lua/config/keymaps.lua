40449afc9aaabdbcfbffa4642fed31624dab63cc
Federico Simonetta <22996003+00sapo@users.noreply.github.com>
2024-06-18 17:13:58 +0200

    Update .config/nvim/lua/config/keymaps.lua Update .config/nvim/lua/utils/utils.lua Update .config/rclone/rclone.conf

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -6,3 +6,9 @@ local utils = require("utils.utils")
 vim.keymap.set("n", "<C-p>", "<cmd>Telescope commands<cr>", { noremap = true, silent = true })
 vim.keymap.set("n", "<leader>sm", "<cmd>Telescope bookmarks list<cr>", { desc = "Select bookmark" })
 vim.keymap.set("n", "<leader><cr>", "<C-/>!!<cr>", { remap = true, desc = "Run command in toggled terminal" })
+vim.keymap.set(
+  "n",
+  "<leader>dL",
+  require("utils.utils").dap_chooser,
+  { noremap = true, desc = "Load dap configuration" }
+)
--- dot_config/nvim/lua/utils/utils.lua
+++ dot_config/nvim/lua/utils/utils.lua
@@ -1,25 +1,24 @@
-local M = {
+local actions_state = require("telescope.actions.state")
+local actions = require("telescope.actions")
 
-  -- A function that lists all json files in this directory and sub directory using Telescope
-  -- And let the user chose one
-  json_picker = function()
-    local opts = {
-      prompt_title = "Select a json file",
-      cwd = vim.fn.expand("."),
-      hidden = true,
-      layout_strategy = "horizontal",
-      layout_config = {
-        width = 0.5,
-        height = 0.5,
-      },
-    }
-    require("telescope.builtin").find_files(opts)
-  end,
+local function load_launchjs(bufnr)
+  local file = actions_state.get_selected_entry()[1]
+  vim.print(file)
+  require("dap.ext.vscode").load_launchjs(file)
+  actions.close(bufnr)
+end
+
+local M = {
 
   -- A function that loads the file chosen by the user with nvim-dap
   dap_chooser = function()
-    local file = M.json_picker()
-    require("dap.ext.vscode").load_launchjs(file)
+    require("telescope.builtin").find_files({
+      attach_mappings = function(_, map)
+        map("n", "<cr>", load_launchjs)
+        map("i", "<cr>", load_launchjs)
+        return true
+      end,
+    })
   end,
 }
 
