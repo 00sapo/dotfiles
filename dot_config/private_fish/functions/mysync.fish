ba7cbda5fa82d84ec6ca9262f72ce54105bfdde4
Federico Simonetta <22996003+00sapo@users.noreply.github.com>
2024-06-18 17:53:03 +0200

    Update .config/fish/functions/mysync.fish

--- dot_config/private_fish/functions/mysync.fish
+++ dot_config/private_fish/functions/mysync.fish
@@ -1,4 +1,4 @@
 function mysync
     command -v task >/dev/null; and task sync
-    command -v chezmoi >/dev/null; and chezmoi update; and chezmoi re-add
+    command -v chezmoi >/dev/null; and chezmoi re-add
 end
