3e3341579fc16ce1a72d824518413a5a28129ebf
Federico Simonetta <22996003+00sapo@users.noreply.github.com>
2024-06-24 16:32:52 +0200

    Update .config/fish/conf.d/99_myconf.fish

--- dot_config/private_fish/conf.d/99_myconf.fish
+++ dot_config/private_fish/conf.d/99_myconf.fish
@@ -4,6 +4,17 @@ set --export PATH "$HOME/.local/bin/:$PATH"
 # asdf
 source $HOME/.asdf/asdf.fish
 
+# brew
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+if command -vq brew
+    if test -d (brew --prefix)"/share/fish/completions"
+        set -p fish_complete_path (brew --prefix)/share/fish/completions
+    end
+    if test -d (brew --prefix)"/share/fish/vendor_completions.d"
+        set -p fish_complete_path (brew --prefix)/share/fish/vendor_completions.d
+    end
+end
+
 # snap
 set --export PATH "/snap/bin:$PATH"
 
