84eead67a9f0157fc6bc269e517cb58532ec16dd
Federico Simonetta <22996003+00sapo@users.noreply.github.com>
2024-06-27 15:55:44 +0200

    Update debianbase.install Update .config/fish/functions/mysync.fish

--- dot_config/private_fish/functions/mysync.fish
+++ dot_config/private_fish/functions/mysync.fish
@@ -1,4 +1,6 @@
 function mysync
     command -v task >/dev/null; and task sync
+    command -v chezmoi >/dev/null; and chezmoi update --apply=false
     command -v chezmoi >/dev/null; and chezmoi re-add
+    command -v chezmoi >/dev/null; and chezmoi apply
 end
