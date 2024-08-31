2bebd593a86b1cb70cf4743884576a08f0f9ff0c
Federico Simonetta <22996003+00sapo@users.noreply.github.com>
2024-08-31 12:38:41 +0200

    Update debianbase.install Update .config/nvim/lazy-lock.json Update .config/fish/config.fish Update .local/bin/open_rgb_auto.sh Update flatpak.install

--- dot_local/bin/open_rgb_auto.sh
+++ dot_local/bin/open_rgb_auto.sh
@@ -5,32 +5,32 @@ ACTIVE_OPTIONS='-m Static -b 20 -c FFFFFF'
 
 # get the active mode from $OPENRGB -l
 active_mode() {
-	cat ~/.cache/openrgb_state | cut -d' ' -f2
+  cat ~/.cache/openrgb_state | cut -d' ' -f2
 }
 
 active_state() {
-	cat ~/.cache/openrgb_state | cut -d' ' -f1
+  cat ~/.cache/openrgb_state | cut -d' ' -f1
 }
 
 # if the file doesn't exist, create it
 if [ ! -f ~/.cache/openrgb_state ]; then
-	echo "1" >~/.cache/openrgb_state
+  echo "1" >~/.cache/openrgb_state
 fi
 
 # toggle state
 # 1 = active
 # 0 = inactive
 # compare strings
-if [ $(active_state) == "0" ]; then
-	echo "1 on" >~/.cache/openrgb_state
-	$OPENRGB $ACTIVE_OPTIONS
+if [ "$(active_state)" = "0" ]; then
+  echo "1 on" >~/.cache/openrgb_state
+  $OPENRGB $ACTIVE_OPTIONS
 else
-	echo "0 off" >~/.cache/openrgb_state
-	$OPENRGB -m "Off"
+  echo "0 off" >~/.cache/openrgb_state
+  $OPENRGB -m "Off"
 fi
 
 active_state=$(active_state)
-while [ $active_state == "1" ]; do
+while [ "$active_state" = "1" ]; do
   # if X11, use xprintidle
   if [ -n "$DISPLAY" ]; then
     # xprintidle for X11 systems
@@ -39,17 +39,17 @@ while [ $active_state == "1" ]; do
     # dbus for wayland
     idle=$(dbus-send --print-reply --dest=org.gnome.Mutter.IdleMonitor /org/gnome/Mutter/IdleMonitor/Core org.gnome.Mutter.IdleMonitor.GetIdletime | awk '/uint64/{print $2}')
   fi
-	if [ $idle -gt $IDLETIME ]; then
-		if [ $(active_mode) == "on" ]; then
-			$OPENRGB -m "Off"
-			echo "1 off" >~/.cache/openrgb_state
-		fi
-	else
-		if [ $(active_mode) == "off" ]; then
-			$OPENRGB $ACTIVE_OPTIONS
-			echo "1 on" >~/.cache/openrgb_state
-		fi
-	fi
-	sleep 0.5
-	active_state=$(active_state) # exits when this script is called again
+  if [ "$idle" -gt $IDLETIME ]; then
+    if [ "$(active_mode)" = "on" ]; then
+      $OPENRGB -m "Off"
+      echo "1 off" >~/.cache/openrgb_state
+    fi
+  else
+    if [ "$(active_mode)" = "off" ]; then
+      $OPENRGB $ACTIVE_OPTIONS
+      echo "1 on" >~/.cache/openrgb_state
+    fi
+  fi
+  sleep 0.5
+  active_state=$(active_state) # exits when this script is called again
 done
