fed1adca9e4512befdccd27e397f5ea537eb31b6
Federico Simonetta <22996003+00sapo@users.noreply.github.com>
2024-09-05 10:33:30 +0200

    Update .config/fish/functions/mysync.fish Change attributes of .config/slimbookbattery/custom/ahorrodeenergia Change attributes of .config/slimbookbattery/custom/equilibrado Change attributes of .config/slimbookbattery/custom/maximorendimiento Change attributes of .config/slimbookbattery/default/ahorrodeenergia Change attributes of .config/slimbookbattery/default/equilibrado Change attributes of .config/slimbookbattery/default/maximorendimiento Change attributes of .config/slimbookbattery/slimbookbattery.conf

--- dot_config/private_fish/functions/mysync.fish
+++ dot_config/private_fish/functions/mysync.fish
@@ -1,5 +1,4 @@
 function mysync
-    command -v task >/dev/null; and task sync
     command -v chezmoi >/dev/null; and chezmoi update --apply=false
     command -v chezmoi >/dev/null; and chezmoi re-add
     command -v chezmoi >/dev/null; and chezmoi apply
