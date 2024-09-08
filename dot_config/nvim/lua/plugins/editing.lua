c92416dc97053505a79b174709b2907befa3cfcf
Federico Simonetta <22996003+00sapo@users.noreply.github.com>
2024-09-08 13:24:56 +0200

    Update .config/nvim/lazy-lock.json Update .config/nvim/lua/config/lazy.lua Update .config/nvim/lua/plugins/editing.lua

--- dot_config/nvim/lua/plugins/editing.lua
+++ dot_config/nvim/lua/plugins/editing.lua
@@ -87,6 +87,55 @@ local M = {
       opts.mapping["<CR>"] = nil
     end,
   },
+  {
+    "folke/zen-mode.nvim",
+    opts = {
+      plugins = {
+        tmux = { enabled = true }, -- disables the tmux statusline
+        todo = { enabled = true }, -- if set to "true", todo-comments.nvim highlights will be disabled
+        alacritty = { enabled = true },
+        options = {
+          enabled = true,
+          ruler = false, -- disables the ruler text in the cmd line area
+          showcmd = false, -- disables the command in the last line of the screen
+          -- you may turn on/off statusline in zen mode by setting 'laststatus'
+          -- statusline will be shown only if 'laststatus' == 3
+          laststatus = 1, -- turn off the statusline in zen mode
+        },
+      },
+      window = {
+        backdrop = 0.95, -- shade the backdrop of the Zen window. Set to 1 to keep the same as Normal
+        -- height and width can be:
+        -- * an absolute number of cells when > 1
+        -- * a percentage of the width / height of the editor when <= 1
+        -- * a function that returns the width or the height
+        width = 0.5, -- width of the Zen window
+        height = 1, -- height of the Zen window
+        -- by default, no options are changed for the Zen window
+        -- uncomment any of the options below, or add other vim.wo options you want to apply
+        options = {
+          signcolumn = "yes", -- disable signcolumn
+          number = false, -- disable number column
+          relativenumber = false, -- disable relative numbers
+          cursorline = false, -- disable cursorline
+          cursorcolumn = false, -- disable cursor column
+          foldcolumn = "0", -- disable fold column
+          list = false, -- disable whitespace characters
+        },
+      },
+      on_open = function(win)
+        -- set spelllang to it-IT
+        vim.api.nvim_set_option_value("spelllang", "it", {})
+        -- disable diagnostics
+        vim.diagnostic.enable(false)
+      end,
+      on_close = function()
+        -- enable diagnostics
+        vim.diagnostic.enable(true)
+      end,
+    },
+    dependencies = { "folke/twilight.nvim" },
+  },
 }
 
 return M
