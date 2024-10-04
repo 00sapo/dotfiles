034ee600058e69d400a519bd4560c6de731c59dd
Federico Simonetta <22996003+00sapo@users.noreply.github.com>
2024-10-04 15:03:30 +0200

    Remove .config/freeplane/1.10.x/accelerator.properties Remove .config/freeplane/1.10.x/accelerators/vim.properties Remove .config/freeplane/1.10.x/addons/freeplane-zotero.script.xml Remove .config/freeplane/1.10.x/addons/freeplane-zotero/lib/freeplane-zotero.jar Remove .config/freeplane/1.10.x/addons/freeplane-zotero/scripts/assignCitationsToNode.groovy Remove .config/freeplane/1.10.x/addons/freeplane-zotero/scripts/refreshCitations.groovy Remove .config/freeplane/1.10.x/addons/freeplane-zotero/scripts/removeCitationsFromNode.groovy Remove .config/freeplane/1.10.x/auto.mmfilter Remove .config/freeplane/1.10.x/auto.properties Remove .config/freeplane/1.10.x/formats.xml Remove .config/freeplane/1.10.x/scanner.xml Remove .config/freeplane/1.10.x/scripts/make_link_from_title.groovy Remove .config/freeplane/1.10.x/single_instance.lock Remove .config/slimbookintelcontroller/slimbookintelcontroller.conf

--- dot_config/freeplane/1.10.x/addons/freeplane-zotero/scripts/refreshCitations.groovy
+++
@@ -1,8 +0,0 @@
-// @ExecutionModes({on_single_node="//main_menu/extras/Zotero"})
-
-import com.petervelosy.freeplanezotero.Zotero
-import static com.petervelosy.freeplanezotero.Constants.*
-
-def zotero = new Zotero(ui, logger, c, map)
-
-zotero.executeApiCommand('refresh', node)
