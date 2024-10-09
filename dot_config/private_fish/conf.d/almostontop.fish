0c86ae619992ebf4cb6117d82512117c5189bc24
Federico Simonetta <22996003+00sapo@users.noreply.github.com>
2024-10-09 23:13:32 +0200

    Remove .config/fish/conf.d/almostontop.fish

--- dot_config/private_fish/conf.d/almostontop.fish
+++
@@ -1,6 +0,0 @@
-function almostontop --on-event fish_preexec
-    # tput cup 0 0 # go to home cursor
-    clear -x       # more portable, avoid clearing the scrollback buffer
-    fish_prompt
-    echo $argv[1] | fish_indent --ansi # colorize executed command
-end
