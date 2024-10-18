64988d83265de6a80b61eb41df0a7b8999d2c504
Federico Simonetta <22996003+00sapo@users.noreply.github.com>
2024-10-18 11:39:51 +0200

    Update .config/fish/config.fish Update .config/fish/fish_plugins

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -17,4 +17,8 @@ if status is-interactive
     command -v thefuck >/dev/null; and thefuck --alias | source
 
     command -v cbonsai >/dev/null; and cbonsai --base (random 1 2) --life (random 10 80) --multiplier (random 2 18) --message (fortune -as)
+
+    if status is-interactive && ! functions -q fisher
+        curl -sL https://git.io/fisher | source && fisher update
+    end
 end
