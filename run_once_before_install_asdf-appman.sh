37fc8d77c7e6db0f92895437b2ff74e0bcb32fee
Federico Simonetta <22996003+00sapo@users.noreply.github.com>
2024-10-31 12:49:52 +0100

    Added installation of asdf and appman

---
+++ run_once_before_install_asdf-appman.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# vim: ft=sh
+
+# 1. install asdf
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf
+
+# 2. install appman
+curl https://raw.githubusercontent.com/ivan-hc/AM/main/AM-INSTALLER | sh
