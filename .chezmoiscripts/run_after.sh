f3b8f0ce7e35ad086517e8e0b483c2772dc3fb5a
Federico Simonetta <22996003+00sapo@users.noreply.github.com>
2024-10-31 13:12:37 +0100

    fixed scripts

---
+++ .chezmoiscripts/run_after.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+# update font cache
+fc-cache
--- .chezmoiscripts/run_once_before_setup.sh
+++ .chezmoiscripts/run_once_before_setup.sh
@@ -6,3 +6,6 @@ git clone https://github.com/asdf-vm/asdf.git ~/.asdf
 
 # 2. install appman
 curl https://raw.githubusercontent.com/ivan-hc/AM/main/AM-INSTALLER | sh
+
+# 3. setup ssh sockets
+mkdir -p ~/.ssh/sockets
