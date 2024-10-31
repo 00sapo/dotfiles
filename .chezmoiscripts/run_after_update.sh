16c3892ebef704ef11eeb69136665eb4338e483b
Federico Simonetta <22996003+00sapo@users.noreply.github.com>
2024-10-31 13:27:24 +0100

    Update .chezmoiexternal.toml Update .chezmoiignore Update .chezmoiscripts/setup.sh Update home/.config/topgrade.toml Change attributes of .chezmoiscripts/update.sh

--- .chezmoiscripts/run_after_update.sh
+++ .chezmoiscripts/run_after_update.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 # update font cache
+echo "Updating font-cache..."
 fc-cache
--- .chezmoiscripts/run_once_before_setup.sh
+++ .chezmoiscripts/run_once_before_setup.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 # vim: ft=sh
 
-# 1. install asdf
+# install asdf
 git clone https://github.com/asdf-vm/asdf.git ~/.asdf
 
-# 2. install appman
+# install appman
 curl https://raw.githubusercontent.com/ivan-hc/AM/main/AM-INSTALLER | sh
 
-# 3. setup ssh sockets
+# install tmux TPM
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+# setup ssh sockets
 mkdir -p ~/.ssh/sockets
