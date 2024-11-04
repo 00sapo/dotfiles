95d740572db2199fa77c1313b3d97c2185144630
Federico Simonetta <22996003+00sapo@users.noreply.github.com>
2024-11-04 14:06:45 +0100

    Update home/.config/fish/conf.d/98_myabbr.fish

--- home/dot_config/private_fish/conf.d/98_myabbr.fish
+++ home/dot_config/private_fish/conf.d/98_myabbr.fish
@@ -10,3 +10,8 @@ abbr -a -- dhe distrobox-host-exec
 abbr -a -- reverse-ssh 'ssh -C -q -N -o ControlMaster=no -R 4833:localhost:4833'
 abbr -a -- defrankestein 'aptitude search -F "%p %O %v %d Dependants:%r" "?narrow(?and(?not(?automatic),?installed),?or(?not(?archive(stable)),?not(?origin(debian))))"'
 abbr -a -- pacman-secupdate 'arch-audit -u | awk "{print $1}" | xargs sudo pacman -Sy'
+abbr -a -- t task
+abbr -a -- ts 'task sync'
+abbr -a -- ta 'task add'
+abbr -a -- td 'task done'
+abbr -a -- tm 'task modify'
