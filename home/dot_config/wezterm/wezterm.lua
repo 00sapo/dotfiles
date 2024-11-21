390742b9ff55e449e9ec5f47c06e82a734e9d573
Federico Simonetta <22996003+00sapo@users.noreply.github.com>
2024-11-21 12:12:22 +0100

    Update home/.config/nvim/lazy-lock.json Update home/.config/nvim/lazyvim.json Update home/.config/wezterm/wezterm.lua

--- home/dot_config/wezterm/wezterm.lua
+++ home/dot_config/wezterm/wezterm.lua
@@ -41,7 +41,7 @@ config.keys = {
 		mods = "CTRL|ALT",
 		action = act.QuickSelectArgs({
 			patterns = {
-				"\\w+?://[.-_&?=,;/\\w]+",
+				"\\w+?://[.\\-_&?=,;/\\w]+",
 			},
 			action = wezterm.action_callback(function(window, pane)
 				local url = window:get_selection_text_for_pane(pane)
