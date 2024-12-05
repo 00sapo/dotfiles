36488fcdf22811c4bf9119ab06f8f10d02b11d95
Your Name <you@example.com>
2024-12-05 11:06:38 +0100

    Removed Appman from init script (it breaks because of prompts)

--- home/.chezmoiscripts/run_once_before_10_presetup.sh
+++ home/.chezmoiscripts/run_once_before_10_presetup.sh
@@ -4,11 +4,12 @@
 echo install asdf
 git clone https://github.com/asdf-vm/asdf.git ~/.asdf
 
-echo install appman
-curl https://raw.githubusercontent.com/ivan-hc/AM/main/AM-INSTALLER | sh
-
 echo install tmux TPM
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 echo setup ssh sockets
 mkdir -p ~/.ssh/sockets
+
+echo install appman:
+echo curl https://raw.githubusercontent.com/ivan-hc/AM/main/AM-INSTALLER | sh
+echo
