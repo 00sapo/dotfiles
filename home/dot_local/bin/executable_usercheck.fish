15c3d722804c2dc57a107c06c8b8b205be58abe0
Federico Simonetta <22996003+00sapo@users.noreply.github.com>
2025-01-28 10:55:11 +0100

    Update home/.local/bin/usercheck.fish

--- home/dot_local/bin/executable_usercheck.fish
+++ home/dot_local/bin/executable_usercheck.fish
@@ -10,39 +10,51 @@ argparse v/verbose -- $argv
 
 # Check if the -v or --version flag is set
 if set -q _flag_v
-    printf "%-20s %-20s\n" User "Last Login"
+    printf "%-20s %-20s %-20s\n" User "Last Login" "Creation Date"
 end
 
 # Loop through each user in /etc/passwd
 for user in (cut -d: -f1 /etc/passwd)
-    # Check if the user is in the groups "student" or "faculty"
+    # Check if the user is in the groups "student" or "faculty" or has a home directory
     set user_groups (id -nG $user)
-    if echo $user_groups | grep -qE '\b(student|faculty)\b'
-        # Get the last login time in seconds since epoch
-        set last_login (lastlog -u $user | awk 'NR==2 {print $4, $5, $6, $9}')
-
-        # if the user never logged in, `last_login` trimmed is `"in**"`
-        if string match -q "in**" $last_login
-            echo "$user has never logged in. You can delete this user with: "
+    # if echo $user_groups | grep -qE '\b(student|faculty)\b'
+    if test -d /home/$user -o (echo $user_groups | grep -cE '\b(student|faculty)\b') -gt 0
+        # if the user has no home, alert
+        if test -z /home/$user
+            echo "$user has no home. You can delete this user with: "
             echo "sudo userdel -r $user"
             continue
         end
 
-        # Convert the last login time to seconds since epoch
-        set last_login_seconds (date -d "$last_login" +%s)
+        # Get the last status change of its home
+        set last_login_seconds (stat -c %Z /home/$user)
 
         # Calculate the difference in seconds
         set diff (math "$current_date-$last_login_seconds")
 
         # If the difference is greater than one year, print the delete command
         if test $diff -gt $one_year_seconds
+            # Convert the last login time to seconds since epoch
+            set last_login (date -d @$last_login_seconds -I)
             echo "User $user doesn't login since $last_login. You can delete this user with: "
+            echo "sudo userdel -r $user"
+        end
+
+        # same with the creation date
+        # Get the user creation date
+        set creation_seconds (stat -c %W /home/$user)
+        set diff_creation (math "$current_date-$creation_seconds")
+        if test $diff_creation -gt $one_year_seconds
+            set creation_date (date -d @$creation_seconds -I)
+            echo "User $user was created on $creation_date. You can delete this user with: "
             echo "sudo userdel -r $user"
         end
 
         # if option -v is used, print $user and $last_login
         if set -q _flag_v
-            printf "%-20s %-20s\n" $user $last_login
+            set last_login (date -d @$last_login_seconds -I)
+            set creation_date (date -d @$creation_seconds -I)
+            printf "%-20s %-20s %-20s\n" $user $last_login $creation_date
         end
     end
 end
