6fd919dbb634329dfc650ee2207bef6edb62bcdb
Federico Simonetta <22996003+00sapo@users.noreply.github.com>
2025-02-24 10:56:25 +0100

    Revert "Update home/.config/nvim/lazy-lock.json"
    This reverts commit 3690c45a72122abf82f3f269615ecd1361d31ad3.

--- home/dot_config/nvim/lua/plugins/ui.lua
+++ home/dot_config/nvim/lua/plugins/ui.lua
@@ -43,4 +43,17 @@ return {
   --   "vigoux/notifier.nvim",
   --   opts = {}, -- needed to force call to setup
   -- },
+  {
+    "snacks.nvim",
+    opts = {
+      -- indent = { enabled = true },
+      -- input = { enabled = true },
+      notifier = { enabled = false },
+      -- scope = { enabled = true },
+      -- scroll = { enabled = true },
+      -- statuscolumn = { enabled = false }, -- we set this in options.lua
+      -- toggle = { map = LazyVim.safe_keymap_set },
+      -- words = { enabled = true },
+    },
+  },
 }
