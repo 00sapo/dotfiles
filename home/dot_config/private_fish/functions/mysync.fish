db094be168bcc90bc980ea9d352bc54880441ccf
Federico Simonetta <22996003+00sapo@users.noreply.github.com>
2025-03-12 09:41:29 +0100

    Update home/.config/fish/functions/mysync.fish Update home/.config/task/taskrc

--- home/dot_config/private_fish/functions/mysync.fish
+++ home/dot_config/private_fish/functions/mysync.fish
@@ -2,5 +2,4 @@ function mysync
     command -v chezmoi >/dev/null; and chezmoi update --apply=false
     command -v chezmoi >/dev/null; and chezmoi re-add
     command -v chezmoi >/dev/null; and chezmoi apply
-    command -v task >/dev/null; and task show taskd.server | grep -q wingtask; and task sync
 end
