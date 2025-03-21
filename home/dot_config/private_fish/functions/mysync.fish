c1151240c40e31c59a7ce5b8a339d4a3e0712d87
Federico Simonetta <22996003+00sapo@users.noreply.github.com>
2025-03-21 15:51:46 +0100

    Update home/debianbase.install Update home/.bashrc Update home/.config/lazygit/config.yml Update home/.config/nvim/lazy-lock.json Update home/.config/fish/functions/mysync.fish Update home/.config/task/taskcheck.toml Update home/.config/task/taskrc Update home/.config/wezterm/wezterm.lua Update home/.ssh/config Update home/.wingtask_certs/taskrc

--- home/dot_config/private_fish/functions/mysync.fish
+++ home/dot_config/private_fish/functions/mysync.fish
@@ -1,31 +1,5 @@
-function unlock_sudo_and_ssh
-    # use pinentry to get a password and store it in a variable
-    set -l pass (systemd-ask-password "Sudo/SSH password:")
-    # unlock sudo
-    echo $pass | sudo -S true
-    # unlock ssh-agent using the $pass
-    # there's a bug in ssh-add? if we give cat to SSH_ASKPASS, it reads from stderr...
-    # need this workaround
-    touch /tmp/pass
-    chmod 700 /tmp/pass
-    echo cat >/tmp/pass
-    echo $pass | SSH_ASKPASS_REQUIRE=force SSH_ASKPASS=/tmp/pass ssh-add
-
-    # erase the password
-    rm /tmp/pass
-    set -e pass
-end
-
-function chezmoi_update
-    chezmoi re-add
-    and chezmoi git fetch origin
-    and chezmoi git rebase origin/main
-    and chezmoi git push
-    and chezmoi apply
-end
-
 function mysync
-    unlock_sudo_and_ssh
-    command -v chezmoi >/dev/null; and chezmoi_update
-    command -v task >/dev/null; and task show taskd.server | grep -q wingtask; and task sync
+    command -v chezmoi >/dev/null; and chezmoi update --apply=false
+    command -v chezmoi >/dev/null; and chezmoi re-add
+    command -v chezmoi >/dev/null; and chezmoi apply
 end
