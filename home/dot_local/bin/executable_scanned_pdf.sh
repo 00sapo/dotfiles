d69e5df1124fffed40d13e7388456cffe3c6291b
Federico Simonetta <22996003+00sapo@users.noreply.github.com>
2025-04-08 19:02:14 +0200

    Update home/.local/bin/scanned_pdf.sh

--- home/dot_local/bin/executable_scanned_pdf.sh
+++ home/dot_local/bin/executable_scanned_pdf.sh
@@ -4,4 +4,4 @@
 #   <policy domain="coder" rights="read | write" pattern="PDF" />
 # inside /etc/ImageMagick-7/policy.xml
 
-convert -density 150 "$1" -colorspace gray -linear-stretch 2.5%x7% -blur 0x0.5 -attenuate 0.25 +noise Gaussian "$2"
+convert -density 150 "$1" -colorspace gray -linear-stretch 2.5%x7% -blur 0x0.1 -attenuate 0.1 +noise Gaussian -contrast -constrast "$2"
