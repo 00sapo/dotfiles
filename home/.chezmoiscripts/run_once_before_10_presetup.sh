ed54b444c77b59bbdab9011d83db00f11e97682d
Federico Simonetta <22996003+00sapo@users.noreply.github.com>
2025-04-17 11:44:22 +0200

    added gpg import keys

--- home/.chezmoiscripts/run_once_before_10_presetup.sh
+++ home/.chezmoiscripts/run_once_before_10_presetup.sh
@@ -9,3 +9,7 @@ git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 echo setup ssh sockets
 mkdir -p ~/.ssh/sockets
+
+echo import GPG keys
+rbw get "GPG-PUBLIC" | gpg --import
+rbw get "GPG-PRIVATE" | gpg --import
