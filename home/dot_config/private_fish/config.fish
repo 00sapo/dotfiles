047280258c60bde5ae865cf8e58ca368b56cb90c
Federico Simonetta <22996003+00sapo@users.noreply.github.com>
2025-05-27 01:27:48 +0200

    Update home/.config/fish/config.fish

--- home/dot_config/private_fish/config.fish
+++ home/dot_config/private_fish/config.fish
@@ -8,6 +8,22 @@ if status is-interactive
     bind -M insert -m default \cc ''
     bind -M visual -m default \cc ''
 
+    function aichat_preparser
+        # Preparse the input for AI chat
+        set -l input (commandline -p)
+        if string match -q '"*' $input
+            if string match -q -- -h $input
+                echo "Usage: aichat [options] [message]"
+                return 0
+            end
+            commandline -r "aichat -e $input"
+        end
+        commandline -f execute
+    end
+    bind -M insert \r aichat_preparser
+    bind -M visual \r aichat_preparser
+    bind -M normal \r aichat_preparser
+
     # zoxide
     command -v zoxide >/dev/null; and zoxide init fish | source
 
@@ -30,8 +46,3 @@ if status is-interactive
     command -v taskcheck >/dev/null; and taskcheck --report today
 
 end
-
-# Added by LM Studio CLI (lms)
-set -gx PATH $PATH /home/federico/.lmstudio/bin
-# End of LM Studio CLI section
-
