f10d185fed39951666201f5f79b3bd02e5dd5869
Federico Simonetta <22996003+00sapo@users.noreply.github.com>
2025-06-03 23:55:38 +0200

    updated script

--- home/.chezmoiscripts/run_once_before_10_presetup.sh
+++ home/.chezmoiscripts/run_once_before_10_presetup.sh
@@ -4,8 +4,8 @@
 echo install soar
 curl -fsSL https://soar.qaidvoid.dev/install.sh | sh
 # adding cargo-bins repository to soar (needed for rbw)
-mkdir -pv "~/.config/soar" &&
-  tee -a "~/.config/soar/config.toml" <<EOF
+mkdir -pv "$HOME/.config/soar" &&
+  tee -a "$HOME/.config/soar/config.toml" <<EOF
 [[repositories]]
 name = "cargo-bins"
 url = "https://meta.pkgforge.dev/external/cargo-bins/x86_64-Linux.json.zstd"
@@ -14,15 +14,17 @@ EOF
 echo install rbw
 soar sync
 soar install rbw
+# fix rbw-agent (it's missing in the cargo-bins repo)
+ln -s "$HOME"/.local/share/soar/packages/rbw-cargo-bins.*/rbw-agent "$HOME"/.local/share/soar/bin/rbw-agent
 # removing cargo-bins repository from soar (avoid conflicts with chezmoi configs)
-rm -f "~/.config/soar/config.toml"
+rm -f "$HOME/.config/soar/config.toml"
 
 echo setup ssh key
-$HOME/.local/share/soar/bin/rbw config set email $(echo "feder" "icosi" "mon" "ett" "a+" "bitwarden" "@" "zoh" "o.com" | sed 's/ //g')
-$HOME/.local/share/soar/bin/rbw config set pinentry pinentry-curses
-$HOME/.local/share/soar/bin/rbw login
+"$HOME"/.local/share/soar/bin/rbw config set email $(echo "feder" "icosi" "mon" "ett" "a+" "bitwarden" "@" "zoh" "o.com" | sed 's/ //g')
+"$HOME"/.local/share/soar/bin/rbw config set pinentry pinentry-curses
+"$HOME"/.local/share/soar/bin/rbw login
 mkdir .ssh
-$HOME/.local/share/soar/bin/rbw get Claude >.ssh/id_rsa
+"$HOME"/.local/share/soar/bin/rbw get Claude >.ssh/id_rsa
 chmod 0400 .ssh/id_rsa
 
 echo install asdf
