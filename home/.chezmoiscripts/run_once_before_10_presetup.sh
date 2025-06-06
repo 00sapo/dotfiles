72a63f5f1cceccfcf4ac87068825f46274b894cf
Federico Simonetta <22996003+00sapo@users.noreply.github.com>
2025-06-06 17:29:57 +0200

    feat: replace appman with flox for rbw installation and setup

--- home/.chezmoiscripts/run_once_before_10_presetup.sh
+++ home/.chezmoiscripts/run_once_before_10_presetup.sh
@@ -2,10 +2,17 @@
 # vim: ft=sh
 
 # installing appman
-echo 2 | wget -qO- https://raw.githubusercontent.com/ivan-hc/AM/main/AM-INSTALLER | sh
+# echo 2 | wget -qO- https://raw.githubusercontent.com/ivan-hc/AM/main/AM-INSTALLER | sh
+# installing flox
+wget "https://downloads.flox.dev/by-env/stable/deb/flox-1.4.3.x86_64-linux.deb"
+sudo apt install -y ./flox-1.4.3.x86_64-linux.deb
+rm flox-1.4.3.x86_64-linux.deb
 
 # installing rbw
-appman -i rbw
+cd "$HOME"
+flox init
+flox activate -d ~ -m run | source
+flox install rbw
 rbw config set email $(echo 'ZmVkMzvCtGUzJCAycmkpUDJjb3NpICFdMG0wKC1vbmV0MiM0dGExMzQrRFZiaXR3MiRePmFyOUsyZGVuUyTCqEB6MG9oJW8uY29tCg==' | base64 -d | sed 's/[^a-z@.+]//g')
 rbw config set pinentry pinentry-curses
 rbw login
