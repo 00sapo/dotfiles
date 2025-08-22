3348555dab9110269272a8db430cc7b623157cfe
Federico Simonetta <22996003+00sapo@users.noreply.github.com>
2025-08-22 12:31:00 +0200

    Update .aider.conf.yml Update .config/nvim/lazy-lock.json Update .config/nvim/lua/plugins/history.lua Update .config/task/taskcheck.toml Update .config/task/taskrc

--- home/dot_config/nvim/lua/plugins/history.lua
+++ home/dot_config/nvim/lua/plugins/history.lua
@@ -4,22 +4,22 @@ local M = {
     keys = { "<space>ts", "<space>tt" },
     cmd = { "UndotreeShow", "UndotreeToggle" },
   },
-  {
-    "kdheepak/lazygit.nvim",
-    cmd = {
-      "LazyGit",
-      "LazyGitConfig",
-      "LazyGitCurrentFile",
-      "LazyGitFilter",
-      "LazyGitFilterCurrentFile",
-    },
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-    },
-    keys = {
-      { "<leader>gg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
-    },
-  },
+  -- {
+  --   "kdheepak/lazygit.nvim",
+  --   cmd = {
+  --     "LazyGit",
+  --     "LazyGitConfig",
+  --     "LazyGitCurrentFile",
+  --     "LazyGitFilter",
+  --     "LazyGitFilterCurrentFile",
+  --   },
+  --   dependencies = {
+  --     "nvim-lua/plenary.nvim",
+  --   },
+  --   keys = {
+  --     { "<leader>gg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+  --   },
+  -- },
   {
     "pgr0ss/vim-github-url",
     cmd = { "GitHubURLRepo", "GitHubURLBlob", "GitHubURLBlame" },
