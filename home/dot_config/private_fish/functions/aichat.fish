cd296ef34e06e5602aece22cf49dc9f45e715400
Federico Simonetta <22996003+00sapo@users.noreply.github.com>
2025-08-26 12:09:51 +0200

    Update .aider.conf.yml Update .aider.model.settings.yml Update .config/fish/functions/aichat.fish

--- home/dot_config/private_fish/functions/aichat.fish
+++ home/dot_config/private_fish/functions/aichat.fish
@@ -1,4 +1,4 @@
-function aichat --wraps='SHELL=fish /home/federico/.local/bin/aichat' --description 'alias aichat SHELL=fish /home/federico/.local/bin/aichat'
-    SHELL=fish /home/federico/.local/bin/aichat $argv
+function aichat --wraps='SHELL=fish /home/federico/.local/share/soar/bin/aichat' --description 'alias aichat SHELL=fish /home/federico/.local/share/soar/bin/aichat'
+    SHELL=fish /home/federico/.local/share/soar/bin/aichat $argv
     history merge
 end
