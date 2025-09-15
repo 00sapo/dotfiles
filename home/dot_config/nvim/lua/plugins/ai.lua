c241534c02c843496bc75afc5f684129cf073729
Federico Simonetta <22996003+00sapo@users.noreply.github.com>
2025-09-15 11:34:53 +0200

    Update .config/nvim/lazy-lock.json Update .config/nvim/lua/plugins/ai.lua Update .config/fish/functions/nvim.fish

--- home/dot_config/nvim/lua/plugins/ai.lua
+++ home/dot_config/nvim/lua/plugins/ai.lua
@@ -1,10 +1,15 @@
 local M = {
   {
+    "MeanderingProgrammer/render-markdown.nvim", -- just to render the codecompanion chat buffer
+    ft = { "markdown", "codecompanion" },
+  },
+  {
     "olimorris/codecompanion.nvim",
     dependencies = {
       "nvim-lua/plenary.nvim",
       "nvim-treesitter/nvim-treesitter",
       "ravitemer/mcphub.nvim",
+      "franco-ruggeri/codecompanion-spinner.nvim",
     },
     cmd = { "CodeCompanion", "CodeCompanionChat", "CodeCompanionActions", "CodeCompanionCmd" },
     keys = {
@@ -32,6 +37,52 @@ local M = {
         adapter = "copilot",
         model = "gpt-4.1",
       },
+      adapters = {
+        http = {
+          deepseek = function()
+            return require("codecompanion.adapters").extend("deepseek", {
+              env = {
+                api_key = "cmd:rbw get deepseek_api_key",
+              },
+            })
+          end,
+          openai = function()
+            return require("codecompanion.adapters").extend("openai", {
+              env = {
+                api_key = "cmd:rbw get openai_api_key",
+              },
+            })
+          end,
+          gemini = function()
+            return require("codecompanion.adapters").extend("gemini", {
+              env = {
+                api_key = "cmd:rbw get gemini_api_key",
+              },
+            })
+          end,
+          openrouter = function()
+            return require("codecompanion.adapters").extend("openrouter", {
+              env = {
+                api_key = "cmd:rbw get openrouter_api_key",
+              },
+            })
+          end,
+          tavily = function()
+            return require("codecompanion.adapters").extend("tavily", {
+              env = {
+                api_key = "cmd:rbw get tavily_api_key",
+              },
+            })
+          end,
+          mistral = function()
+            return require("codecompanion.adapters").extend("mistral", {
+              env = {
+                api_key = "cmd:rbw get mistral_api_key",
+              },
+            })
+          end,
+        },
+      },
       extensions = {
         mcphub = {
           callback = "mcphub.extensions.codecompanion",
@@ -41,6 +92,7 @@ local M = {
             show_result_in_chat = true,
           },
         },
+        spinner = {},
       },
     },
   },
