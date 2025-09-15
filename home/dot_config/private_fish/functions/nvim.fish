c9878af9855bd6b86b44351dbe502b0bfa557cc6
Federico Simonetta <22996003+00sapo@users.noreply.github.com>
2025-09-15 10:39:51 +0200

    Update .config/nvim/lazy-lock.json Update .config/fish/functions/nvim.fish

--- home/dot_config/private_fish/functions/nvim.fish
+++ home/dot_config/private_fish/functions/nvim.fish
@@ -2,14 +2,47 @@ function nvim
     set keys deepseek_api_key gemini_api_key openai_api_key mistral_api_key openrouter_api_key tavily_api_key brave_api_key
     set nvim_bin (which nvim 2>/dev/null)
 
-    for key in $keys
-        set varname (string upper $key)
-        if not set -q $varname
-            # Run rbw in background and export the variable
-            begin
-                set -x $varname (rbw get $key 2>/dev/null)
-            end &
+    if contains -- -k $argv
+        set start_time (date +%s%3N) # milliseconds
+
+        rbw unlock
+
+        set tempdir (mktemp -d)
+        set pids
+
+        for key in $keys
+            set -l varname (string upper $key)
+            if not set -q $varname
+                rbw get $key 2>/dev/null >$tempdir/$varname &
+                set -a pids $last_pid
+            end
         end
+
+        for pid in $pids
+            wait $pid
+        end
+
+        for key in $keys
+            set -l varname (string upper $key)
+            if test -f $tempdir/$varname
+                set -x $varname (cat $tempdir/$varname)
+            end
+        end
+
+        rm -rf $tempdir
+
+        set end_time (date +%s%3N)
+        set elapsed (math "$end_time - $start_time")
+        echo "Key setup took $elapsed ms"
+
+        # Remove -k from argv
+        set new_argv
+        for arg in $argv
+            if test $arg != -k
+                set new_argv $new_argv $arg
+            end
+        end
+        set argv $new_argv
     end
 
     $nvim_bin $argv
