4c92ea4ecabcb692b0c9d4e76c58e552edac0edc
Federico Simonetta <22996003+00sapo@users.noreply.github.com>
2025-09-16 10:26:49 +0200

    Update .config/nvim/lua/plugins/ai.lua Update .ssh/config

--- home/dot_config/nvim/lua/plugins/ai.lua
+++ home/dot_config/nvim/lua/plugins/ai.lua
@@ -30,10 +30,108 @@ local M = {
         desc = "Add Visual Selection",
       },
     },
+    config = function(_, opts)
+      require("codecompanion").setup(opts)
+      local default_system_prompt = require("codecompanion.config").strategies.chat.opts.system_prompt
+      require("codecompanion.config").strategies.chat.opts.system_prompt = function(...)
+        -- default system prompt can be called only after setup
+        local default = default_system_prompt(...)
+        -- replace CodeCompanion with Eleonora
+        return default:gsub("CodeCompanion", "Eleonora")
+      end
+    end,
     opts = {
       chat = {
         adapter = "copilot",
         model = "gpt-4.1",
+      },
+      strategies = {
+        chat = {
+          roles = {
+            ---The header name for the LLM's messages
+            llm = function(adapter)
+              return "Eleonora (" .. adapter.formatted_name .. ")"
+            end,
+            ---The header name for your messages
+            user = "Federico",
+          },
+          tools = {
+            opts = {
+              default_tools = {
+                "vectorcode_toolbox", -- from vectorcode extension
+                "full_stack_dev",
+                "fetch_webpage",
+                "search_web",
+              },
+            },
+          },
+        },
+      },
+      display = {
+        chat = {
+          intro_message = "Welcome to Eleonora 🌳! Press ? for options",
+        },
+      },
+      memory = {
+        default = {
+          description = "Collection of common files for all projects",
+          parser = "claude",
+          files = {
+            "ai/CONVENTIONS.md",
+            "ai/DESIGN.md",
+          },
+        },
+        opts = {
+          chat = {
+            enabled = true,
+          },
+        },
+      },
+      adapters = {
+        http = {
+          deepseek = function()
+            return require("codecompanion.adapters").extend("deepseek", {
+              env = {
+                api_key = "cmd:rbw get deepseek_api_key",
+              },
+            })
+          end,
+          openai = function()
+            return require("codecompanion.adapters").extend("openai", {
+              env = {
+                api_key = "cmd:rbw get openai_api_key",
+              },
+            })
+          end,
+          gemini = function()
+            return require("codecompanion.adapters").extend("gemini", {
+              env = {
+                api_key = "cmd:rbw get gemini_api_key",
+              },
+            })
+          end,
+          openrouter = function()
+            return require("codecompanion.adapters").extend("openrouter", {
+              env = {
+                api_key = "cmd:rbw get openrouter_api_key",
+              },
+            })
+          end,
+          tavily = function()
+            return require("codecompanion.adapters").extend("tavily", {
+              env = {
+                api_key = "cmd:rbw get tavily_api_key",
+              },
+            })
+          end,
+          mistral = function()
+            return require("codecompanion.adapters").extend("mistral", {
+              env = {
+                api_key = "cmd:rbw get mistral_api_key",
+              },
+            })
+          end,
+        },
       },
       extensions = {
         mcphub = {
@@ -59,6 +157,11 @@ local M = {
         },
         history = {
           enabled = true,
+          picker_keymaps = {
+            rename = { n = "r", i = "<C-s>" },
+            delete = { n = "d", i = "<C-w>" },
+            duplicate = { n = "<C-y>", i = "<C-y>" },
+          },
         },
       },
     },
