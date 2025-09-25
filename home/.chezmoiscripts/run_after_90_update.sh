0119eae328afd5fe45be28b976ee11b2b6493a99
Federico Simonetta <22996003+00sapo@users.noreply.github.com>
2025-09-25 10:31:34 +0200

    fix: fc-cache may bot be available

--- home/.chezmoiscripts/run_after_90_update.sh
+++ home/.chezmoiscripts/run_after_90_update.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
+if command -v fc-cache >/dev/null 2>&1; then
+  echo "Font cache tool found."
+else
+  echo "Font cache tool not found. Please install fontconfig."
+  exit 1
+fi
 echo "Updating font-cache..."
 fc-cache
