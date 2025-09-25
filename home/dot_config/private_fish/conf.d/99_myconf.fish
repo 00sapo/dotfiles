895fdee76da472d69926401872cc5015f768de6e
Federico Simonetta <22996003+00sapo@users.noreply.github.com>
2025-09-25 17:02:10 +0200

    Update .config/fish/conf.d/99_myconf.fish

--- home/dot_config/private_fish/conf.d/99_myconf.fish
+++ home/dot_config/private_fish/conf.d/99_myconf.fish
@@ -1,6 +1,19 @@
 # basic
 set --export PATH "$HOME/.local/bin:$HOME/.local/share/soar/bin/:$PATH"
 
+# ASDF configuration code
+if test -z $ASDF_DATA_DIR
+    set _asdf_shims "$HOME/.asdf/shims"
+else
+    set _asdf_shims "$ASDF_DATA_DIR/shims"
+end
+# Do not use fish_add_path (added in Fish 3.2) because it
+# potentially changes the order of items in PATH
+if not contains $_asdf_shims $PATH
+    set -gx --prepend PATH $_asdf_shims
+end
+set --erase _asdf_shims
+
 # editor
 if command -v nvim >/dev/null
     set --export EDITOR (which nvim)
