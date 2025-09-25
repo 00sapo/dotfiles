977849b8cb3863e44d23ae6a8347dd6b374b8026
Federico Simonetta <22996003+00sapo@users.noreply.github.com>
2025-09-25 10:04:39 +0200

    moving to stew as main home-based package manager

--- home/.chezmoiscripts/run_once_before_10_presetup.sh
+++ home/.chezmoiscripts/run_once_before_10_presetup.sh
@@ -1,25 +1,24 @@
 #!/bin/sh
 # vim: ft=sh
 
-echo install soar
-curl -fsSL https://soar.qaidvoid.dev/install.sh | sh
-# adding cargo-bins repository to soar (needed for rbw)
-soar defconfig --external
-mkdir -pv "$HOME/.config/soar"
-tee -a "$HOME/.config/soar/config.toml" <<EOF
-[[repositories]]
-name = "cargo-bins"
-url = "https://meta.pkgforge.dev/external/cargo-bins/x86_64-Linux.json.zstd"
-EOF
+echo installing stew
+if command -v dpkg >/dev/null 2>&1; then
+  arch=$(dpkg --print-architecture)
+else
+  arch=$(uname -m)
+fi
+curl -s https://api.github.com/repos/marwanhawari/stew/releases/latest |
+  grep -E "https://.*/stew-.*-linux-amd64.tar.gz" |
+  cut -d : -f 2,3 |
+  tr -d \" |
+  wget -qi -
+tar -xf stew*.tar.gz
+mv stew ~/.local/bin
+rm stew*
 
-echo install rbw
-export PATH="$HOME/.local/bin/:$PATH"
-soar sync
-soar install rbw
-# fix rbw-agent (it's missing in the cargo-bins repo)
-ln -s "$HOME"/.local/share/soar/packages/rbw-cargo-bins.*/rbw-agent "$HOME"/.local/share/soar/bin/rbw-agent
-# removing cargo-bins repository from soar (avoid conflicts with chezmoi configs)
-rm -f "$HOME/.config/soar/config.toml"
+echo installing from stew applications from lock file
+export PATH="$HOME/.local/bin:$PATH"
+stew install "$(dirname 0)/../dot_config/stew/Stewfile.lock.json"
 
 echo setup ssh key
 "$HOME"/.local/share/soar/bin/rbw config set email $(echo "feder" "icosi" "mon" "ett" "a+" "bitwarden" "@" "zoh" "o.com" | sed 's/ //g')
