4107b1d9e87382da6ceb44304b720d7c4b0ea32f
Federico Simonetta <22996003+00sapo@users.noreply.github.com>
2025-09-25 10:26:23 +0200

    fix:presetup contained wrong stefile lock path

--- home/.chezmoiscripts/run_once_before_10_presetup.sh
+++ home/.chezmoiscripts/run_once_before_10_presetup.sh
@@ -18,7 +18,7 @@ rm stew*
 
 echo installing from stew applications from lock file
 export PATH="$HOME/.local/bin:$PATH"
-stew install "$HOME/.local/share/chezmoi/home/dot_config/stew/Stewfile.lock.json"
+stew install "$HOME/.local/share/chezmoi/home/dot_local/share/stew/Stewfile.lock.json"
 
 echo setup ssh key
 rbw config set email $(echo "feder" "icosi" "mon" "ett" "a+" "bitwarden" "@" "zoh" "o.com" | sed 's/ //g')
