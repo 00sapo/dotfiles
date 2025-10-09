a332df165b71529f8ac7de5790293b7be369e9cf
Federico Simonetta <22996003+00sapo@users.noreply.github.com>
2025-10-09 23:14:19 +0200

    Update .config/fish/conf.d/99_myconf.fish Update .local/share/stew/Stewfile.lock.json

--- home/dot_config/private_fish/conf.d/99_myconf.fish
+++ home/dot_config/private_fish/conf.d/99_myconf.fish
@@ -1,5 +1,5 @@
 # basic
-set --export PATH "$HOME/.local/bin:$HOME/.local/share/soar/bin/:$PATH"
+set --export PATH "$HOME/.local/bin:$PATH"
 
 # ASDF configuration code
 if test -z $ASDF_DATA_DIR
@@ -20,33 +20,24 @@ if command -v nvim >/dev/null
     set --export SYSTEM_EDITOR (which nvim)
 end
 
-# ruby
-set --export PATH "$HOME/.local/share/gem/ruby/3.0.0/bin:$PATH"
-
 # javascript
 set --export PATH "$HOME/.npm-global/bin:$PATH"
 
 # java
 set --export JAVA_HOME ""
 
-# pyenv
-set -x PATH "$HOME/.pyenv/bin:$PATH"
-set -Ux PYENV_ROOT $HOME/.pyenv
-set -U fish_user_paths $PYENV_ROOT/bin $fish_user_paths
-command -v pyenv >/dev/null; and status is-interactive; and pyenv init --path | source
-
-# golang
-if not set -q GOBIN
-    set --export GOBIN "$HOME/go/bin"
-end
-# set --export GOROOT /usr/lib/go/
-set --export PATH "$GOBIN:$PATH"
-set --export GOCACHE "$HOME/go/cache"
-# set --export GOPATH "$HOME/go"
-# set --export GOMODCACHE "$HOME/go/pkg/mod"
-
-# rust
-set --export PATH "$HOME/.cargo/bin:$PATH"
+# # golang
+# if not set -q GOBIN
+#     set --export GOBIN "$HOME/go/bin"
+# end
+# # set --export GOROOT /usr/lib/go/
+# set --export PATH "$GOBIN:$PATH"
+# set --export GOCACHE "$HOME/go/cache"
+# # set --export GOPATH "$HOME/go"
+# # set --export GOMODCACHE "$HOME/go/pkg/mod"
+#
+# # rust
+# set --export PATH "$HOME/.cargo/bin:$PATH"
 
 # mlflow
 # set --export MLFLOW_TRACKING_URI "$HOME/mlruns"
@@ -54,8 +45,8 @@ set --export PATH "$HOME/.cargo/bin:$PATH"
 # ladspa
 set --export LADSPA_PATH "$LADSPA_PATH:/usr/lib/ladspa"
 
-# Apptainer
-set --export PATH "$HOME/.local/opt/apptainer-relocatable/bin/:$PATH"
+# # Apptainer
+# set --export PATH "$HOME/.local/opt/apptainer-relocatable/bin/:$PATH"
 
 # nvim
 # set --export NVIM_LISTEN_ADDRESS "/tmp/nvimsocket"
