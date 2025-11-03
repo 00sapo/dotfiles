9909fb47224602a9aced29428bd50a1c59a46932
Federico Simonetta <22996003+00sapo@users.noreply.github.com>
2025-11-03 23:32:56 +0100

    Update .config/wezterm/wezterm.lua

--- home/dot_config/wezterm/wezterm.lua
+++ home/dot_config/wezterm/wezterm.lua
@@ -67,31 +67,12 @@ config.keys = {
 		mods = "CTRL|ALT",
 		action = act.SplitVertical,
 	},
+  {
+    key = 'e',
+    mods = 'CTRL|ALT',
+    action = wezterm.action.CharSelect
+  },
 }
-
--- allow nvim-zenmode to setup wezterm
-wezterm.on("user-var-changed", function(window, pane, name, value)
-	local overrides = window:get_config_overrides() or {}
-	if name == "ZEN_MODE" then
-		local incremental = value:find("+")
-		local number_value = tonumber(value)
-		if incremental ~= nil then
-			while number_value > 0 do
-				window:perform_action(wezterm.action.IncreaseFontSize, pane)
-				number_value = number_value - 1
-			end
-			overrides.enable_tab_bar = false
-		elseif number_value < 0 then
-			window:perform_action(wezterm.action.ResetFontSize, pane)
-			overrides.font_size = nil
-			overrides.enable_tab_bar = true
-		else
-			overrides.font_size = number_value
-			overrides.enable_tab_bar = false
-		end
-	end
-	window:set_config_overrides(overrides)
-end)
 
 -- plugins
 require('plugins').apply_to_config(config)
