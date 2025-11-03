f3c9f2220459fe9ae452fab26a4c0ea01a135a18
Federico Simonetta <22996003+00sapo@users.noreply.github.com>
2025-11-03 10:52:17 +0100

    Update .config/lazygit/config.yml Update .config/nvim/plugin/30_mini.lua Update .config/nvim/plugin/40_plugins.lua Update .config/task/taskrc

--- home/dot_config/nvim/plugin/30_mini.lua
+++ home/dot_config/nvim/plugin/30_mini.lua
@@ -34,7 +34,7 @@ local now_if_args = _G.Config.now_if_args
 -- - `:h mini.nvim-color-schemes` - list of other color schemes
 -- - `:h MiniHues-examples` - how to define highlighting with 'mini.hues'
 -- - 'plugin/40_plugins.lua' honorable mentions - other good color schemes
-now(function() vim.cmd('colorscheme miniwinter') end)
+-- now(function() vim.cmd('colorscheme miniwinter') end)
 
 -- You can try these other 'mini.hues'-based color schemes (uncomment with `gcc`):
 -- now(function() vim.cmd('colorscheme minispring') end)
